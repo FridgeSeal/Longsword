c052baddf8221b1d3e46ac8cf52977c69e56b7d4
Tom Watson <tom_w@fastmail.com>
2021-03-07 15:53:53 +1100

    Add an initial index structure that searches across all provided docs

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use crate::pipeline;
 use bimap::BiMap;
 use itertools::{Itertools, Unique};
+use slab::Slab;
 // use fst::{
 //     automaton::{Str, Subsequence, Union},
 //     map::Stream,
@@ -12,10 +13,69 @@ use unicode_segmentation::{UnicodeSegmentation, UnicodeWords};
 
 type Dictionary = BiMap<String, usize>;
 type SentenceArray = Vec<Vec<usize>>;
+
+#[derive(Debug)]
+pub struct SearchResult<'a> {
+    pub doc_name: &'a str,
+    pub n_hits: usize,
+    // pub results: &'a [&'a String],
+    pub results: Vec<String>,
+}
+
+impl Display for SearchResult<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "Document name: {}\nNumber of occurrences: {}\nSamples: {:#?}",
+            self.doc_name, self.n_hits, self.results
+        )
+    }
+}
+
+pub struct Index {
+    name: String,
+    // config: ????
+    // statistics: Polars df
+    // bloom_filter:
+    documents: Slab<Document>,
+    keys: Vec<usize>,
+}
 pub struct Document {
     pub name: String,
     pub dictionary: Dictionary,
     sentence_set: SentenceArray,
+}
+
+impl Index {
+    pub fn new(name: impl Into<String>, data: Vec<Document>) -> Self {
+        let name = name.into();
+        let mut documents = Slab::with_capacity(data.len() * 2);
+        let keys: Vec<usize> = data.into_iter().map(|f| documents.insert(f)).collect();
+        Self {
+            name,
+            documents,
+            keys,
+        }
+    }
+    pub fn search(&self, search_term: &str) -> anyhow::Result<Vec<SearchResult>> {
+        let s_lower = search_term.to_lowercase();
+        let s = s_lower.unicode_words().unique().collect::<Vec<&str>>();
+        let results = self
+            .keys
+            .iter()
+            .map(|k| {
+                let doc: &Document = self.documents.get(*k).unwrap();
+                let srch_res = doc.search(&s);
+                SearchResult {
+                    doc_name: &doc.name,
+                    n_hits: srch_res.len(),
+                    // results: srch_res.iter().take(5).collect::<Vec<&String>>().as_slice(),
+                    results: srch_res.into_iter().take(5).collect(),
+                }
+            })
+            .collect();
+        Ok(results)
+    }
 }
 
 impl Document {
@@ -81,10 +141,9 @@ impl Document {
         }
     }
 
-    pub fn search(&self, s: &str) -> Vec<String> {
-        info!("Provided search data: {}", s);
-        let s = s.to_lowercase().unicode_words().unique();
-        s.map(|t| self.search_for_term(t))
+    pub fn search(&self, s: &[&str]) -> Vec<String> {
+        s.iter()
+            .map(|t| self.search_for_term(t))
             .filter(|x| x.len() > 0)
             .flatten()
             .collect()
